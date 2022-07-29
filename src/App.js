8384945eb62e26f3dffac4781511c30959660f05
인소현 <sohyun7305@naver.com>
2022-07-30 03:16:56 +0900

    SigninPage 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 // 컴포넌트 연결
-import Signin from './components/Signin';
-import Signup from './components/Signup';
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 import Friend from "./components/Friend";
 
 //스타일 관련
-import './App.css';
-
-
+import "./App.css";
 
 function App() {
   return (
-    
     <div className="App">
       <Routes>
         {/* 홈 화면 - SigninPage */}
         <Route index element={<Signin />} />
-        <Route path="/signup" element={ <Signup /> } />
-        <Route path="/:my_id/friendList" element={ <Friend /> } />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/:my_id/friendList" element={<Friend />} />
       </Routes>
-     
     </div>
   );
 }
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 // 로고 이미지
 import Logo from "../talk_logo.png";
@@ -8,53 +8,76 @@ import Logo from "../talk_logo.png";
 // 스타일 관련
 import styled from "styled-components";
 
-
-
 const Signin = () => {
   return (
-    <>
-      {/* Logo */}
-      <img
-        src={Logo}
-        alt="logo"
-        style={{
-          width: "133px",
-          height: "133px",
-          margin: "136px auto",
-          marginBottom: "8%",
-        }}
-      />
-      <SigninFormBox>
-        {/* ID */}
-        <IdInput placeholder="카카오계정(이메일)" />
-        {/* PW */}
-        <PwInput placeholder="비밀번호" />
-        {/* Signin Button - Friend 페이지로 이동 */}
-        <SigninBtn>로그인</SigninBtn>
-      </SigninFormBox>
+    <Wrap>
+      <ContentsWrap>
+        <div style={{ width: "133px", height: "133px", marginBottom: "40px" }}>
+          {/* Logo */}
+          <img
+            src={Logo}
+            alt="logo"
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        </div>
+        <SigninFormBox>
+          {/* ID */}
+          <IdInput placeholder="카카오계정(이메일)" />
+          {/* PW */}
+          <PwInput placeholder="비밀번호" />
+          {/* Signin Button - Friend 페이지로 이동 */}
+          <SigninBtn>로그인</SigninBtn>
+        </SigninFormBox>
 
-      <GoingToSignupBox>
-        <GoingToSignupText>
-          계정이 없으신가요?
-          {/* Signup 페이지로 이동 */}
-          <Link to="/signup" style={{ textDecoration: "none" }}>
-            <span>회원가입</span>
-          </Link>  
-        </GoingToSignupText>
-      </GoingToSignupBox>
-    </>
+        <GoingToSignupBox>
+          <GoingToSignupText>
+            계정이 없으신가요?
+            {/* Signup 페이지로 이동 */}
+            <Link to="/signup" style={{ textDecoration: "none" }}>
+              <span>회원가입</span>
+            </Link>
+          </GoingToSignupText>
+        </GoingToSignupBox>
+      </ContentsWrap>
+    </Wrap>
   );
 };
 
 // styled components
+
+const Wrap = styled.div`
+  max-width: 320px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 auto;
+`;
+
+const ContentsWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
 const SigninFormBox = styled.div`
-  width: 230px;
-  height: 95px;
+  width: 90%;
+  height: 100%;
   margin: 0% auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 const IdInput = styled.input`
-  width: 230px;
+  width: 100%;
   height: 46px;
   border: 1px solid #d2d2d2;
   background-color: #fff;
@@ -66,7 +89,7 @@ const IdInput = styled.input`
 `;
 
 const PwInput = styled.input`
-  width: 230px;
+  width: 100%;
   height: 46px;
   border: 1px solid #d2d2d2;
   background-color: #fff;
@@ -78,7 +101,7 @@ const PwInput = styled.input`
 `;
 
 const SigninBtn = styled.button`
-  width: 230px;
+  width: 100%;
   height: 50px;
   background: #38302b;
   color: #fae300;
@@ -90,9 +113,8 @@ const SigninBtn = styled.button`
 `;
 
 const GoingToSignupBox = styled.div`
-  width: 224px;
+  width: 90%;
   height: 51px;
-  margin: 16% auto;
   line-height: 51px;
   font-size: 0.7rem;
 `;
@@ -101,7 +123,6 @@ const GoingToSignupText = styled.p`
   span {
     font-weight: 900;
     font-size: 0.75rem;
-    margin-left: 3%;
     color: #575757;
   }
   color: #7d7d7d;
